86be90a6462224053844af7fc1d820ccea8c1fbb
Michael smickles Carver <xiberphim@gmail.com>
2014-06-06 15:45:44 -0700

    Begin functional test.

---
+++ thing/tests/test_functional_test.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class TestCaseTradeTool(TestCase):
+
+    def test_basic_usage(self):
+        # Alice logs in
+        # She navigates to the trade tool
+        # She enters here current location
+        # The tool tells her what to buy, where to buy it and where to sell it
+        self.fail('finish test')
