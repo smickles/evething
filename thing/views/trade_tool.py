700d2e9a4e6d5ec61e116f81850b8334ea8a9ba7
Michael smickles Carver <xiberphim@gmail.com>
2014-06-06 17:42:46 -0700

    Add Trade Tool page with a non-functioning system input box.

--- thing/views/trade_tool.py
+++ thing/views/trade_tool.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from thing.stuff import render_page
 
 @login_required()
 def trade_tool(request):
-    return
+    out = render_page(
+        'thing/trade_tool.html',    # template
+        {},                         # data
+        request                     # request
+    )
+    return out
