e2e223bb7a7f45fbd6b5b0d8c9b23cbe02ce93f6
Michael smickles Carver <xiberphim@gmail.com>
2014-06-09 22:23:07 -0700

    Add to trade tool behavior. Begin trade_plan transfer framework.

--- thing/tests/test_functional_test.py
+++ thing/tests/test_functional_test.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from itertools import chain
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from thing.tests.common_things import login
 
 class TestCaseTradeTool(TestCase):
@@ -21,11 +23,37 @@ class TestCaseTradeTool(TestCase):
         # The page has a similar style to the other pages
         self.assertIsNotNone(self.browser.find_element_by_name('viewport'))
         self.assertIsNotNone(self.browser.find_element_by_class_name('navbar-header'))
+        # The page does not have any trade plan elements yet
+        with self.assertRaises(NoSuchElementException):
+            self.browser.find_element_by_tag_name('h2')
         
         # She enters here current location
         system_input = self.browser.find_element_by_name('system')
-        system_input.send_keys('Jita')
+        system_input.send_keys('test')
         system_input.submit()
         
         # The tool tells her what to buy, where to buy it and where to sell it
+        result_header = self.browser.find_element_by_tag_name('h2')
+        self.assertEqual(result_header.text, 'Best Trade')
+
+        table_headers = self.browser.find_elements_by_tag_name('th')
+        table_cells = self.browser.find_elements_by_tag_name('td')
+        mock_table_data = (
+            'Item',
+            'Pickup',
+            'Drop Off',
+            'Buy Up To',
+            'Amount',
+            'Sell Down To',
+            'Tritanium',
+            'Jita 4-4 Caldari Business Tribunal Information Center',
+            'Amarr 6-2 Theology Council Tribunal',
+            6.36,
+            44000,
+            7.63
+        )
+
+        for cell in chain(table_headers, table_cells):
+            self.assertIn(cell.text, mock_table_data)
+
         self.fail('finish test')
--- thing/views/trade_tool.py
+++ thing/views/trade_tool.py
@@ -1,11 +1,35 @@
 from django.contrib.auth.decorators import login_required
+import sys
 from thing.stuff import render_page
 
 @login_required()
 def trade_tool(request):
-    out = render_page(
-        'thing/trade_tool.html',    # template
-        {},                         # data
-        request                     # request
-    )
-    return out
+    if system = request.GET.get('system'):
+
+        trade_plan = make_trade_plan(system)
+
+        out = render_page(
+            'thing/trade_tool.html',    # template
+            {'trade_plan': trade_plan}, # data
+            request                     # request
+        )
+        return out
+    else:
+        out = render_page(
+            'thing/trade_tool.html',    # template
+            {},                         # data
+            request                     # request
+        )
+        return out
+
+def make_trade_plan(system):
+    if system == 'test':
+        trade_plan = (
+            ('Tritanium',                                               # Item
+            'Jita 4-4 Caldari Business Tribunal Information Center',    # Pickup
+            'Amarr 6-2 Theology Council Tribunal',                      # Drop off
+            6.36,                                                       # Max Buy
+            44000,                                                      # Amount
+            7.63),                                                      # Min Sell
+        )
+        return trade_plan
